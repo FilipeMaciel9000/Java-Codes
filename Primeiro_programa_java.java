f5d3324385eafe609c7fbb4a837af6626bcaf091
FilipeMaciel9000 <83095857+FilipeMaciel9000@users.noreply.github.com>
2021-04-26 21:29:53 -0300

    Add files via upload

---
+++ Primeiro_programa_java.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiro_programa_java;
+
+/**
+ *
+ * @author Filipe
+ */
+public class Primeiro_programa_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Seu codigo em java será escrito aqui:
+        System.out.println("Olá mundo");
+        System.out.println("Pronto. Me livrei da maldição!");
+        System.out.println("Primeiro progrma em Java feito com sucesso!!!");
+    }
+    
+}
