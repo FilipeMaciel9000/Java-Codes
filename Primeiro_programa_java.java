cefbd582108825af3bbe8f5dd56f3fe14397d14a
FilipeMaciel9000 <83095857+FilipeMaciel9000@users.noreply.github.com>
2021-07-01 10:48:52 -0300

    Update Primeiro_programa_java.java

--- Primeiro_programa_java.java
+++ Primeiro_programa_java.java
@@ -3,20 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package primeiro_programa_java;
+package primeiro_programa_java; //(1)
 
-/**
- *
- * @author Filipe Rios Mariz Maciel
- */
-public class Primeiro_programa_java {
+/**Comentário documental com Java
+ * @author: Filipe Rios Maciel Maciel
+ * Entenda a estrutura do código do seu código Java:
+ * 1) Pacotes que serão utilizados durante nossa aplicação Java.
+ * 2) Cabeçalho da classe e agrupador de instruções.
+ * 3) Instrução principal [Main] estática [static] que não retorna valor nenhum [void]; pela qual pode ser passado valores do tipo string.
+ * 4) Comando para escrever algo na tela, pulando uma linha em Java.
+*/
+
+public class Primeiro_programa_java { //(2)
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { //(3)
         //Seu codigo em java será escrito aqui:
-        System.out.println("Olá mundo");
+        System.out.println("Olá mundo"); //(4)
         System.out.println("Pronto. Me livrei da maldição!");
         System.out.println("Primeiro progrma em Java feito com sucesso!!!");
     }
