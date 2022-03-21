0619bc121f1b7eb27828dea9e85e7014016c51b2
Filipe Maciel <83095857+FilipeMaciel9000@users.noreply.github.com>
2022-03-21 14:17:45 -0300

    Update Primeiro_programa_java.java

--- Primeiro_programa_java.java
+++ Primeiro_programa_java.java
@@ -1,15 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * Comentário documental com Java
+/** Comentário documental com Java
+ * @goal: Converte temperaturas de Fahrenheit para Celsius;
  * @author: Filipe Rios Maciel Maciel
- * Entenda a estrutura do código do seu código Java:
+ * @date: 22/01/2022
+ * * *
+ * @algorithm
+ * Escrever "Olá mundo" na tela
+ * * *
+ * @declarations: 
  * 1) Pacotes que serão utilizados durante nossa aplicação Java.
  * 2) Cabeçalho da classe e agrupador de instruções.
  * 3) Instrução principal [Main] estática [static] que não retorna valor nenhum [void]; pela qual pode ser passado valores do tipo string.
- * 4) Comando para escrever algo na tela, pulando uma linha em Java.
-*/
+ * 4) Comando para escrever algo na tela e pular uma linha em Java é, System.out.println(""); esse ponto e virfula é muito importante, não se esqueça.
+ * * *
+ * @notes:
+ * 1) No NetBeans o atalho para System.out.println(""); é = digitar sout e depois apertar Tab;
+ * 2) No Eclipse: o atalho para System.out.println(""); é = digitar sysout e apertar ctrl espaço;
+ */
 
 package primeiro_programa_java; //(1)
 
@@ -17,9 +23,11 @@ public class Primeiro_programa_java { //(2)
 
     public static void main(String[] args) { //(3)
         //Seu codigo em java será escrito aqui:
-        System.out.println("Olá mundo"); //(4)
-        System.out.println("Pronto. Me livrei da maldição!");
-        System.out.println("Primeiro progrma em Java feito com sucesso!!!");
+        // Toda a logica do codigo aqui:
+        System.out.println(">>> Olá mundo!");//(4)
+        System.out.println(">>> Pronto já me livrei da maldição do olá mundo");
+        System.out.println(">>> Primeiro progrma em Java feito com sucesso");
+        System.out.println(">>> Vamos programar pesado!!! ");
     }
     
 }
